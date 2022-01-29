a2137028a759b47f532cee9389739073bfe601ef
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-29 15:58:27 +0530

    Update brav.sh

--- brav.sh
+++ brav.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 while :
 do
-timeout -k 40 30 brave-browser-stable --no-sandbox --disable-dev-shm-usage --disable-gpu --no-default-browser-check --disable-client-side-phishing-detection --single-process --deterministic-mode "https://github.com/developeranaz"
-sleep 5
+cd /
+rclone copy 1:dotbrav/brav.tar.gz /brav.tar.gz
+tar -xf /brav.tar.gz
+timeout -k 1500 1510 brave-browser-stable --no-sandbox --disable-dev-shm-usage --disable-gpu --no-default-browser-check --disable-client-side-phishing-detection --single-process --deterministic-mode "https://github.com/developeranaz"
+curl -L "$SELF"
+rm /brav.tar.gz
+tar -vcf /brav /brav.tar.gz
+rclone copy /brav.tar.gz 1:dotbrav
+
 done
